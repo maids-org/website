d748bc0701bad9417720c8cfe9809e0c1c012bad
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-03-12 20:40:57 +0500

    initial state website

---
+++ pages/contact.js
@@ -0,0 +1,19 @@
+import Page from '@components/page'
+
+const Contact = () => {
+  return (
+    <Page title="Contact" footer={false} description="Get in touch.">
+      <article>
+        <p>Get in touch.</p>
+
+        <blockquote>
+          <a href="mailto:support@maid.uz?subject=Hello there!&body=We've Been Trying To Reach You About Your Car's Extended Warranty" className="reset">
+            support@maid.uz
+          </a>
+        </blockquote>
+      </article>
+    </Page>
+  )
+}
+
+export default Contact
---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import Page from '@components/page'
+import Link from '@components/link'
+
+const About = () => {
+  return (
+    <Page description="Hello there, we are Maids. Started as a group of developers which later became a network for communities consisting tech enthusiasts.">
+      <article>
+        <h1>Maids. (ex Mad Maids)</h1>
+
+        <p>
+          Started as a group of developers which later{' '}
+          became a network for communities & teams{' '}
+          consisting tech enthusiasts. We govern{' '}
+          <Link underline href="/community">
+          many communities & teams
+          </Link>{' '}
+          in Uzbek IT segment.
+        </p>
+
+        <p>
+          Working with{' '}
+          <Link underline href="https://udc.org.uz" external>
+            ᚯ Uzbek Developers Consortium
+          </Link>{' '}
+          to standardize uzbek IT communities and bring{' '}
+          best community experience.
+        </p>
+      </article>
+    </Page>
+  )
+}
+
+export default About
